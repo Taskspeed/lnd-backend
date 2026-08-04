5cb20132a5ad9302871180e4a2ca57b3f1a2d230
Taskspeed <taskspeed2002@gmail.com>
2026-08-04 13:15:14 +0800

    added: status

--- database/migrations/2026_08_04_042435_create_events_table.php
+++ database/migrations/2026_08_04_042435_create_events_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('type_name')->nullable();
             $table->string('source_name')->nullable();
             $table->string('speaker_name')->nullable();
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
