1be44d6bc77e26c5b14c6e524438535ca5508a02
Taskspeed <taskspeed2002@gmail.com>
2026-08-05 13:54:53 +0800

    update: route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,72 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Event\EventController;
+use App\Http\Controllers\Event\Library\EventModeController;
+use App\Http\Controllers\Event\Library\EventSourceController;
+use App\Http\Controllers\Event\Library\EventTitleController;
+use App\Http\Controllers\Event\Library\EventTypeController;
+use App\Http\Controllers\Event\Library\EventVenueController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::prefix('user')->group(function () {
+        Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware(['auth:sanctum']);
+        Route::post('/register', [AuthController::class, 'register']);
+    });
+
+    Route::prefix('type')->group(function () {
+        Route::get('/index', [EventTypeController::class, 'index']);
+        Route::post('/store', [EventTypeController::class, 'store']);
+        Route::put('/update/{typeId}', [EventTypeController::class, 'update']);
+        Route::delete('/delete/{typeId}', [EventTypeController::class, 'destroy']);
+    });
+
+    Route::prefix('mode')->group(function () {
+        Route::get('/index', [EventModeController::class, 'index']);
+        Route::post('/store', [EventModeController::class, 'store']);
+        Route::put('/update/{modeId}', [EventModeController::class, 'update']);
+        Route::delete('/delete/{modeId}', [EventModeController::class, 'destroy']);
+    });
+
+    Route::prefix('source')->group(function () {
+        Route::get('/index', [EventSourceController::class, 'index']);
+        Route::post('/store', [EventSourceController::class, 'store']);
+        Route::put('/update/{sourceId}', [EventSourceController::class, 'update']);
+        Route::delete('/delete/{sourceId}', [EventSourceController::class, 'destroy']);
+    });
+
+    Route::prefix('title')->group(function () {
+        Route::get('/index', [EventTitleController::class, 'index']);
+        Route::post('/store', [EventTitleController::class, 'store']);
+        Route::put('/update/{titleId}', [EventTitleController::class, 'update']);
+        Route::delete('/delete/{titleId}', [EventTitleController::class, 'destroy']);
+    });
+
+
+    Route::prefix('venue')->group(function () {
+        Route::get('/index', [EventVenueController::class, 'index']);
+        Route::post('/store', [EventVenueController::class, 'store']);
+        Route::put('/update/{venueId}', [EventVenueController::class, 'update']);
+        Route::delete('/delete/{venueId}', [EventVenueController::class, 'delete']);
+    });
+
+     Route::prefix('event')->group(function () {
+        Route::get('/index', [EventController::class, 'index']);
+        Route::post('/store', [EventController::class, 'store']);
+        // Route::put('/update/{event}', [EventController::class, 'update']);
+        Route::get('view/{eventId}', [EventController::class, 'view']);
+        Route::delete('/delete/{eventId}', [EventController::class, 'destory']);
+    });
+
 });
