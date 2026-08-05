583ddf130c95a6072bbbbc3004d478c03a2ddb03
Taskspeed <taskspeed2002@gmail.com>
2026-08-05 13:54:44 +0800

    add: column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('control_no')->nullable();
             $table->string('username')->unique();
+            $table->string('office')->nullable();
             // $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
