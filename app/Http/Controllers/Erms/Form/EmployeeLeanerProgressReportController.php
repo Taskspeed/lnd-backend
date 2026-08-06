92c57f0ad6fa427f12accd20c695393e7404605f
Taskspeed <taskspeed2002@gmail.com>
2026-08-06 16:01:36 +0800

    added: controller form employee

---
+++ app/Http/Controllers/Erms/Form/EmployeeLeanerProgressReportController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Erms\Form;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeLeanerProgressReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
