8e79fd5c217a3e7ced0365d01bfef3ac4e87e7f8
Taskspeed <taskspeed2002@gmail.com>
2026-08-26 13:23:39 +0800

    modified: model

--- app/Models/RSP/vwEmployee.php
+++ app/Models/RSP/vwEmployee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\RSP;
 
+
+use App\Models\RSP\xTraining;
 use Illuminate\Database\Eloquent\Model;
 
 class vwEmployee extends Model
@@ -9,4 +11,11 @@ class vwEmployee extends Model
     //
     protected $connection = 'second_db';
     protected $table = 'vwEmployee'; 
+
+    public function xTraining(){ return $this->hasMany(
+            xTraining::class,
+            'ControlNo', // foreign key on xTrainings table
+            'ControlNo'  // local key on vwEmployee table
+        );
+    }
 }
