ab3ff1a8149d959a57e0725e47fecfb1c923c613
Taskspeed <taskspeed2002@gmail.com>
2026-08-26 13:23:08 +0800

    category service

---
+++ app/Services/Event/Library/EventCategoryService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services\Event\Library;
+
+use App\Models\Event\Library\EventCategory;
+use Illuminate\Support\Facades\DB;
+
+class EventCategoryService
+{
+    public function index()
+    {
+        $category = EventCategory::all();
+        return $category;
+    }
+
+    public function create(array $validateData)
+    {
+        return DB::transaction(function () use ($validateData) {
+            $category = EventCategory::create($validateData);
+
+            if (!$category) {
+                throw new \Exception('Failed to create event category');
+            }
+
+            return $category;
+        });
+    }
+
+    public function update(int $categoryId, array $validateData)
+    {
+        return DB::transaction(function () use ($categoryId, $validateData) {
+            $category = EventCategory::find($categoryId);
+
+            if (!$category) {
+                throw new \Exception('Event category not found');
+            }
+
+            $category->update($validateData);
+
+            return $category;
+        });
+    }
+
+    public function destroy(int $categoryId)
+    {
+        $category = EventCategory::find($categoryId);
+
+        if (!$category) {
+            throw new \Exception('Event category not found');
+        }
+
+        $category->delete();
+
+        return $category;
+    }
+}
