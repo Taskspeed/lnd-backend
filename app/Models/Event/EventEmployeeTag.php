005693a4972422468d62188d3f4f249175d3ca2d
Taskspeed <taskspeed2002@gmail.com>
2026-09-03 15:27:54 +0800

    add employee model tag

---
+++ app/Models/Event/EventEmployeeTag.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Event;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventEmployeeTag extends Model
+{
+    //
+
+    protected $table = 'employee_tags'; 
+
+    protected $fillable = [
+        'event_schedule_id',
+        'control_no',
+        'name',
+        'position',
+        'office',
+        'status'
+    ];
+}
