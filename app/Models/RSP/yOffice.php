1ad189e76a76b7d22738789d25818945a004f9ec
Taskspeed <taskspeed2002@gmail.com>
2026-09-22 08:22:03 +0800

    add: model offices

---
+++ app/Models/RSP/yOffice.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\RSP;
+
+use Illuminate\Database\Eloquent\Model;
+
+class yOffice extends Model
+{
+    //
+  protected $connection = 'second_db';
+
+    protected $table = 'yOffice'; 
+}
