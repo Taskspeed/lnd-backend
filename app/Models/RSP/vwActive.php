7a817f2faa0df6e601bc4f8b1bd51ca9866619dd
Taskspeed <taskspeed2002@gmail.com>
2026-09-25 08:14:57 +0800

    add model

---
+++ app/Models/RSP/vwActive.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\RSP;
+
+use Illuminate\Database\Eloquent\Model;
+
+class vwActive extends Model
+{
+    //
+
+    protected $connection  = 'second_db'; 
+
+    protected $table = 'vwActive';
+}
